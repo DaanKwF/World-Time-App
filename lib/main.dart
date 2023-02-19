5027bf40da16d141c771c21a15a0d41c4258792d
DaanKwF <daan@kashmirworldfoundation.org>
2023-02-19 22:11:22 +0100

    Basic routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/location.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     );
   }
 }
